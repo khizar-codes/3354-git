8f6fafc28803f251ccb354738b1f69274a127a48
Muhammad Khizar <mkhizar2001@gmail.com>
2020-09-19 19:40:55 -0500

    Updated information files 1 The information files have been modified to better reflect the owner of the repository. Added repository owner information to README.md and deleted DELETEME.txt
    Updated information files
    1
    The information files have been modified to better reflect the owner of the repository.
    Added repository owner information to README.md and deleted DELETEME.txt

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,17 +1,23 @@
 package adder;
 
-public class Main {
+public class Main 
+{
 
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) 
+    {
+        try 
+        {
             int result = addArguments(args);
             System.out.println(result);
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             System.err.println("Please provide two integers to add");
         }
     }
 
-    private static int addArguments(String[] args) {
+    private static int addArguments(String[] args) 
+    {
         return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
     }
 }
