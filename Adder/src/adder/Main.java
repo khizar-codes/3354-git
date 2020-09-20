7bf22584fe73d04720c47ff3b2779372b669d23d
Muhammad Khizar <mkhizar2001@gmail.com>
2020-09-20 17:20:25 -0500

    Main.java changes
    Does negative substraction

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -2,16 +2,40 @@ package adder;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) 
+    {
+        try 
+        {
             int result = addArguments(args);
             System.out.println(result);
-        } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+        } 
+        catch (Exception e) 
+        {
+            System.err.println("Please provide at least two integers to add");
         }
     }
 
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+    private static int addArguments(String[] args) 
+    {
+    	int total = 0;
+    	
+    	if(args[0].contains("-"))
+    	{
+    		for(int i = 1; i < args.length; i++)
+        	{
+        		total = total + Integer.valueOf(args[i]);
+        	}
+    		
+    		total = (total * -1);
+    	}
+    	else
+    	{
+    		for(int i = 0; i < args.length; i++)
+        	{
+        		total = total + Integer.valueOf(args[i]);
+        	}
+    	}
+        
+    	return total;
     }
 }
